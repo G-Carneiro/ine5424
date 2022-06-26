6a8ae3d69c9f6f5acdc681ebac1d11bf4f970397
G-Carneiro <carneiro.gabr@gmail.com>
2022-06-26 19:18:15 -0300

    att thread_init.cc

--- src/api/thread_init.cc
+++ src/api/thread_init.cc
@@ -12,19 +12,26 @@ extern "C" { void __epos_app_entry(); }
 void Thread::init()
 {
     db<Init, Thread>(TRC) << "Thread::init()" << endl;
+    CPU::smp_barrier();
 
-    Criterion::init();
+    if (CPU::mhartid() == 0) {
+        Criterion::init();
 
-    typedef int (Main)();
+        typedef int (Main)();
 
-    // If EPOS is a library, then adjust the application entry point to __epos_app_entry, which will directly call main().
-    // In this case, _init will have already been called, before Init_Application to construct MAIN's global objects.
-    Main * main = reinterpret_cast<Main *>(__epos_app_entry);
+        // If EPOS is a library, then adjust the application entry point to __epos_app_entry, which will directly call main().
+        // In this case, _init will have already been called, before Init_Application to construct MAIN's global objects.
+        Main *main = reinterpret_cast<Main *>(__epos_app_entry);
 
-    new (SYSTEM) Thread(Thread::Configuration(Thread::RUNNING, Thread::MAIN), main);
+        new(SYSTEM) Thread(Thread::Configuration(Thread::RUNNING, Thread::MAIN), main);
 
-    // Idle thread creation does not cause rescheduling (see Thread::constructor_epilogue)
-    new (SYSTEM) Thread(Thread::Configuration(Thread::READY, Thread::IDLE), &Thread::idle);
+        // Idle thread creation does not cause rescheduling (see Thread::constructor_epilogue)
+        new(SYSTEM) Thread(Thread::Configuration(Thread::READY, Thread::IDLE), &Thread::idle);
+    } else {
+        new (SYSTEM) Thread(Thread::Configuration(Thread::RUNNING, Thread::IDLE), &Thread::idle);
+    }
+
+    CPU::smp_barrier();
 
     // The installation of the scheduler timer handler does not need to be done after the
     // creation of threads, since the constructor won't call reschedule() which won't call
@@ -32,12 +39,20 @@ void Thread::init()
     // Letting reschedule() happen during thread creation is also harmless, since MAIN is
     // created first and dispatch won't replace it nor by itself neither by IDLE (which
     // has a lower priority)
-    if(Criterion::timed)
+    if(Criterion::timed && (CPU::mhartid() == 0))
         _timer = new (SYSTEM) Scheduler_Timer(QUANTUM, time_slicer);
 
     // No more interrupts until we reach init_end
-    CPU::int_disable();
+    // CPU::int_disable();
+    // Install an interrupt handler to receive forced reschedules
+    if (smp) {
+        if(CPU::mhartid() == 0)
+            IC::int_vector(IC::INT_RESCHEDULER, rescheduler);
+        CPU::int_disable();
+        IC::enable(IC::INT_RESCHEDULER);
+    }
 
+    CPU::smp_barrier();
     // Transition from CPU-based locking to thread-based locking
     This_Thread::not_booting();
 }
